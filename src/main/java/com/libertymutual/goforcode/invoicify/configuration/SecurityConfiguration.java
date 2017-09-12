e9a134107939ac0c1b94ab8f5caadb1fdc74620d
Joshua Holt <joshua.holt@libertymutual.com>
2017-09-12 15:26:50 -0700

    first commit - working requirements for initial assignment

---
+++ src/main/java/com/libertymutual/goforcode/invoicify/configuration/SecurityConfiguration.java
@@ -0,0 +1,37 @@
+package com.libertymutual.goforcode.invoicify.configuration;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+
+@EnableWebSecurity
+public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
+    
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+            .authorizeRequests()
+                .antMatchers("/", "/loginalot", "/css/**", "/js/**").permitAll()
+                .antMatchers("/admin").hasRole("ADMIN")
+                .antMatchers("/invoices/**").hasAnyRole("ADMIN", "ACCOUNTANT")
+                .antMatchers("/billing-records/**").hasAnyRole("ADMIN", "CLERK")
+                .anyRequest().authenticated()
+                .and()
+            .formLogin()
+                .loginPage("/loginalot");
+    }
+    
+    @Bean
+    public UserDetailsService userDetailsService() {
+        InMemoryUserDetailsManager manager = new InMemoryUserDetailsManager();
+        manager.createUser(User.withUsername("admin").password("admin").roles("ADMIN").build());
+        manager.createUser(User.withUsername("clerk").password("clerk").roles("CLERK").build());
+        manager.createUser(User.withUsername("accountant").password("accountant").roles("ACCOUNTANT").build());
+        return manager;
+    }
+    
+}
